5414e669d3a564518999a992b4bdc92d78a7b32e
Anjali Upadhyay <upadhyayangel9999@gmail.com>
2024-03-17 17:11:35 +0530

    datatypes

---
+++ 01_Basics/data_types.js
@@ -0,0 +1,20 @@
+"use strict" // Treat all JS code as newer version
+
+// alert(3+3) // We are using nodejs , not browser
+
+let name = "Hitesh"
+let age = 18
+
+let isLoggedIn = false
+
+// number = 2 to power 53
+// bigint for storing large 
+//string = ""
+// Boolean = true/ false
+// null = standalone value
+// undefined = value not defined
+// Symbol => unique
+// object
+
+console.log(typeof undefined); // Undefined
+console.log(typeof null);      // Object
