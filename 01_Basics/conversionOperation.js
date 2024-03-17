63526f0b613cb5f2c8a3b1918b73962cfdb05ea8
Anjali Upadhyay <upadhyayangel9999@gmail.com>
2024-03-17 17:41:44 +0530

    Converion Of datatypes

---
+++ 01_Basics/conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "33";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+
+// Conversion in number
+// "33"  converted into 33
+// "33abc" => NaN (Not a number)
+// true => 1 and false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false 
+// "" => false
+// "Anjali" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
