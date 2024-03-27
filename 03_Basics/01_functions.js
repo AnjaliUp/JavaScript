4d2817effb3899526c7317f973627cfcde2f2643
Anjali Upadhyay <upadhyayangel9999@gmail.com>
2024-03-27 12:55:19 +0530

    Functions part 1 done

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -35,5 +35,37 @@ function loginUserMessage(username){
 }
 
 // console.log(loginUserMessage("Anjali"))
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+
+
+function calculateCartPrice(...num1){
+    return num1;
+}
+
+// console.log(calculateCartPrice(200,400,500));
+
+const user = {
+    username:"Anjali",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "Sam",
+    price: 399
+})
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,500,1000]));
 
